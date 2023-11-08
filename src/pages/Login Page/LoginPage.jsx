597913ad517295a56ee70f2f7e90fef2e8ea75d3
Jaden-77 <jadenceniza77@gmail.com>
2023-11-08 18:07:31 +0800

    Updated Login Page

--- src/pages/Login Page/LoginPage.jsx
+++ src/pages/Login Page/LoginPage.jsx
@@ -2,72 +2,117 @@ import { Component, useState } from 'react'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import '../Login Page/LoginDesign.css'
+import { Link } from 'react-router-dom/cjs/react-router-dom';
 
 class LoginPage extends Component {
-  constructor() {
-    super();
-    this.state = {
-      email: '',
-      password: '',
-      error: '',
-    };
+  constructor(props) {
+      super(props);
+      this.state = {
+          showPassword: false,
+      };
   }
 
-  handleInputChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
-  }
-
-  handleLogin = () => {
-    const { email, password } = this.state;
-    const data = { email, password };
-
-    // Send a POST request to your backend API for authentication using fetch
-    fetch('/api/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        // Handle successful login, e.g., store tokens or redirect
-      })
-      .catch((error) => {
-        this.setState({ error: 'Invalid email or password' });
-      });
-  }
+  togglePassword = () => {
+      this.setState((prevState) => ({
+          showPassword: !prevState.showPassword,
+      }));
+  };
 
   render() {
-    return (
-      <div>
-        <h1>Login Page</h1>
-        <form>
-          <div className="mb-3">
-            <label htmlFor="email" className="form-label">Email</label>
-            <input
-              type="email"
-              className="form-control"
-              id="email"
-              name="email"
-              onChange={this.handleInputChange}
-            />
+      const inputType = this.state.showPassword ? 'text' : 'password';
+      const eyeIcons = this.state.showPassword
+          ? 'bi bi-eye-slash-fill'
+          : 'bi bi-eye-fill';
+
+      return (
+          <div>
+              <nav className="navbar bg-dark navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+                  <div className="container-fluid">
+                      <a className="navbar-brand" href="#">
+                          CampusChime
+                      </a>
+                      <button
+                          className="navbar-toggler"
+                          type="button"
+                          data-bs-toggle="collapse"
+                          data-bs-target="#navbarNav"
+                          aria-controls="navbarNav"
+                          aria-expanded="false"
+                          aria-label="Toggle navigation"
+                      >
+                          <span className="navbar-toggler-icon"></span>
+                      </button>
+                      <div className="collapse navbar-collapse" id="navbarNav">
+                          <ul className="navbar-nav">
+                              <li className="nav-item"></li>
+                          </ul>
+                      </div>
+                  </div>
+              </nav>
+
+              <div
+                  class="card mb-3"
+                  style={{
+                      maxWidth: '30%',
+                      position: 'relative',
+                      left: '33%',
+                      top: '250px',
+                  }}
+              >
+                  <div class="row g-0">
+                      <div
+                          class="col-md-4"
+                          style={{ backgroundColor: 'gray' }}
+                      ></div>
+                      <div class="col-md-8">
+                          <div class="card-body">
+                              <h1>Login Page</h1>
+
+                              <form action="" method="post">
+                                  <label htmlFor="loginUsername">Username</label>
+                                  <input
+                                      name="loginUsername"
+                                      class="form-control"
+                                      type="text"
+                                      placeholder="Enter your Username"
+                                      aria-label="default input example"
+                                  />
+
+                                  <label htmlFor="loginPassword">Password</label>
+                                  <br />
+                                  <div className="showPassword">
+                                      <input
+                                          name="loginPassword"
+                                          class="form-control"
+                                          type={inputType}
+                                          placeholder="Enter your Password"
+                                          aria-label="default input example"
+                                      />
+                                      <i
+                                          className={eyeIcons}
+                                          onClick={this.togglePassword}
+                                      ></i>
+                                  </div>
+                                  <br />
+
+                                  <br />
+                                  <button
+                                      type="submit"
+                                      class="btn btn-secondary"
+                                  >
+                                      Login
+                                  </button>
+                              </form>
+
+                              Don't have an account?{' '}
+                              <Link to="/register">Register here</Link>
+                          </div>
+                      </div>
+                  </div>
+              </div>
           </div>
-          <div className="mb-3">
-            <label htmlFor="password" className="form-label">Password</label>
-            <input
-              type="password"
-              className="form-control"
-              id="password"
-              name="password"
-              onChange={this.handleInputChange}
-            />
-          </div>
-          {this.state.error && <div className="alert alert-danger">{this.state.error}</div>}
-          <button type="button" className="btn btn-primary" onClick={this.handleLogin}>Login</button>
-        </form>
-      </div>
-    );
+      );
   }
 }
 
