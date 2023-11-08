7b3a0571c4d19cb8cdac5db30b6de9f8ac1640f2
Lorenz Gil Suico <19101360@usc.edu.ph>
2023-11-08 23:41:54 +0800

    Updated Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import { Link } from 'react-router-dom/cjs/react-router-dom';
+import WebLogo from '../assets/CampusChimePurple.png'
 
 class Navbar extends Component {
 
@@ -20,24 +21,28 @@ class Navbar extends Component {
     render() {
         return (
             <div>
-                <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
-                    <div class="container-fluid">
-                        <a class="navbar-brand" href="#">Navbar</a>
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
+                <nav className="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+                    <div className="container-fluid">
+                        <a className="navbar-brand" href="#"> <img src={WebLogo} alt="" style={{ height: '70px', width: '80px' }} /> CampusChime</a>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
                         </button>
-                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                                <li class="nav-item" style={{color: 'white'}}>
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li className="nav-item" style={{ color: 'white' }}>
                                     Home
                                 </li>
-                                <li class="nav-item" style={{color: 'white'}}>
+                                <li className="nav-item" style={{ color: 'white' }}>
                                     About Us
                                 </li>
-                                <li class="nav-item" style={{color: 'white'}}>
+                                <li className="nav-item" style={{ color: 'white' }}>
                                     Entrepreneur
                                 </li>
                             </ul>
+                        </div>
+                        <div className='d-flex'>
+                            <Link to='/LoginPage'><button type="button" className="btn btn-secondary">Login Account</button></Link>&nbsp;
+                            <Link to='/RegisterPage'><button type="button" className="btn btn-secondary">Regiter Account</button></Link>
                         </div>
                     </div>
                 </nav>
