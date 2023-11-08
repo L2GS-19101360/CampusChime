c58fd2bfca1101156a07c8019854283438ba49f0
Lorenz Gil Suico <19101360@usc.edu.ph>
2023-11-08 17:25:25 +0800

    New AboutUsPage.jsx

---
+++ src/pages/About Us Page/AboutUsPage.jsx
@@ -0,0 +1,30 @@
+import { Component, useState } from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import { Link } from 'react-router-dom/cjs/react-router-dom';
+
+class AboutUsPage extends Component{
+
+    constructor() {
+        super();
+    }
+
+    componentDidMount() {
+
+    }
+    componentWillUnmount() {
+
+    }
+
+    render() {
+        return (
+            <div>
+                About Us Page
+            </div>
+        )
+    }
+
+}
+
+export default AboutUsPage
