cabafca1df92f27b4f38aa2111e85b6d21167bbd
Jaden-77 <jadenceniza77@gmail.com>
2023-11-11 01:24:43 +0800

    added carousel to landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,53 @@
-import { Component, useState } from 'react'
-import './App.css'
+import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import Navbar from './components/Navbar';
+import "bootstrap-icons/font/bootstrap-icons.css";
+import Navbar from "./components/Navbar";
+import { Carousel } from "react-bootstrap";
+import './App.css';
+
 
 class App extends Component {
-
   constructor() {
     super();
   }
 
-  componentDidMount() {
+  componentDidMount() {}
 
-  }
-  componentWillUnmount() {
+  componentWillUnmount() {}
 
-  }
+
+
+  
 
   render() {
     return (
       <div>
         <Navbar />
-      </div>
-    )
-  }
 
+        <Carousel style={{ marginTop: '92px' }}>
+          <Carousel.Item>
+            {/* Add content for the first slide */}
+            <img
+              className="d-block w-100"
+              src="https://example.com/slide1.jpg"
+              alt="First slide"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            {/* Add content for the second slide */}
+            <img
+              className="d-block w-100"
+              src="https://example.com/slide2.jpg"
+              alt="Second slide"
+            />
+          </Carousel.Item>
+        </Carousel>
+
+
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
