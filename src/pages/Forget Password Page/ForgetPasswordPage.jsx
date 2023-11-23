8a42a5f574a6a1905fdca5d512cfa41968949fd1
Jaden-77 <jadenceniza77@gmail.com>
2023-11-23 18:40:17 +0800

    forgetpassword page frontend

--- src/pages/Forget Password Page/ForgetPasswordPage.jsx
+++ src/pages/Forget Password Page/ForgetPasswordPage.jsx
@@ -1,59 +1,98 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { Link, withRouter } from "react-router-dom";
-import {
-    Form,
-    Button,
-    Alert,
-    Navbar,
-    Nav,
-    FloatingLabel,
-} from "react-bootstrap";
-import axios from "axios";
-import "bootstrap-icons/font/bootstrap-icons.css";
+import { Navbar, Nav, Form, Button, FloatingLabel } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import EnvelopeFill from "react-bootstrap-icons/dist/icons/envelope-fill";
 import WebLogo from "../../assets/CampusChimePurple.png";
 import Logo from "../../assets/CampusChime.png";
 
 class ForgetPasswordPage extends Component {
+  constructor() {
+    super();
+  }
 
-    constructor() {
-        super();
-    }
+  componentDidMount() {}
 
-    componentDidMount() {
+  componentWillUnmount() {}
 
-    }
-    componentWillUnmount() {
+  render() {
+    return (
+      <div>
+        <Navbar bg="dark" expand="lg" variant="dark">
+          <Navbar.Brand href="/">
+            <img
+              src={WebLogo}
+              alt=""
+              style={{ height: "70px", width: "80px" }}
+            />{" "}
+            CampusChime
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarNav" />
+          <Navbar.Collapse id="navbarNav">
+            <Nav className="mr-auto"></Nav>
+          </Navbar.Collapse>
+        </Navbar>
 
-    }
-
-    render() {
-        return (
-            <div>
-
-                <Navbar bg="dark" expand="lg" variant="dark">
-                    <Navbar.Brand href="/">
-                        <img
-                            src={WebLogo}
-                            alt=""
-                            style={{ height: "70px", width: "80px" }}
-                        />{" "}
-                        CampusChime
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="navbarNav" />
-                    <Navbar.Collapse id="navbarNav">
-                        <Nav className="mr-auto"></Nav>
-                    </Navbar.Collapse>
-                </Navbar>
-
-                {/* Jaden, Create the Front-End of the Page below this comment */}
-
-                
-
+        <div
+          className="card mb-3"
+          style={{
+            backgroundColor: "white",
+            maxWidth: "43%",
+            position: "relative",
+            left: "30%",
+            top: "240px",
+          }}
+        >
+          <div className="row g-0">
+            <div
+              className="col-md-4"
+              style={{ backgroundColor: "gray", textAlign: "center" }}
+            >
+              <img
+                src={Logo}
+                alt=""
+                style={{ height: "160px", width: "160px" }}
+              />
             </div>
-        );
-    }
+            <div className="col-md-8">
+              <div className="card-body">
+                <h1>Forget Password</h1>
+                <p>
+                  Please enter your email address to receive a password reset
+                  link.
+                </p>
+                <Form style={{ marginTop: "20px" }}>
+                  <FloatingLabel
+                    controlId="forgetPasswordEmail"
+                    label="Email address"
+                  >
+                    <Form.Control type="email" placeholder="Email" required />
+                    <EnvelopeFill
+                      size={20}
+                      style={{
+                        position: "absolute",
+                        top: "20px",
+                        right: "10px",
+                      }}
+                    />
+                  </FloatingLabel>
 
+                  <Button
+                    style={{ marginTop: "15px", marginBottom: "10px" }}
+                    variant="outline-primary"
+                    type="submit"
+                  >
+                    Reset Password
+                  </Button>
+                </Form>
+                Already Have an Account? &nbsp;
+                <Link to="/LoginPage">Login here</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default ForgetPasswordPage
+export default ForgetPasswordPage;
