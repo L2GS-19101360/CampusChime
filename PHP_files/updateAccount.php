e03c2bf3ad7c0c3f0169219cfa28e47581327f7f
Lorenz Gil Suico <19101360@usc.edu.ph>
2023-11-28 10:06:47 +0800

    Still need to fix

--- PHP_files/updateAccount.php
+++ PHP_files/updateAccount.php
@@ -1,54 +1,7 @@
 <?php
-// Include your database connection file
-include_once("db_connection.php");
 
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, UPDATE");
-header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+include("db_connection.php");
 
-// Check if the required parameters are set
-if (
-    isset($_GET['lastname']) &&
-    isset($_GET['firstname']) &&
-    isset($_GET['contactnumber']) &&
-    isset($_GET['email']) &&
-    isset($_GET['password']) &&
-    isset($_GET['user_id'])
-) {
-    // Sanitize and store the parameters
-    $lastname = mysqli_real_escape_string($conn, $_GET['lastname']);
-    $firstname = mysqli_real_escape_string($conn, $_GET['firstname']);
-    $contactnumber = mysqli_real_escape_string($conn, $_GET['contactnumber']);
-    $email = mysqli_real_escape_string($conn, $_GET['email']);
-    $password = mysqli_real_escape_string($conn, $_GET['password']);
-    $customer_id = mysqli_real_escape_string($conn, $_GET['user_id']);
 
-    // Hash the password (you should use a stronger hashing method in production)
-    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-    // Update the row in the database
-    $query = "UPDATE users SET 
-        lastname = '$lastname',
-        firstname = '$firstname',
-        contactnumber = '$contactnumber',
-        email = '$email',
-        password = '$hashed_password'
-        WHERE user_id = '$customer_id'";
-
-    $result = mysqli_query($conn, $query);
-
-    if ($result) {
-        // Update successful
-        echo "Update successful!";
-    } else {
-        // Update failed
-        echo "Update failed: " . mysqli_error($conn);
-    }
-} else {
-    // Required parameters not set
-    echo "Missing required parameters!";
-}
-
-// Close the database connection
-mysqli_close($conn);
 ?>
--- src/pages/User Setting Page/UserSettingPage.jsx
+++ src/pages/User Setting Page/UserSettingPage.jsx
@@ -68,36 +68,18 @@ class UserSettingPage extends Component {
 
   handleSubmit = async (e) => {
     e.preventDefault();
-
+  
     if (this.state.newPassword === this.state.confirmPassword) {
-      try {
-        const response = await fetch(`http://localhost/campuschime/PHP_files/updateAccount.php?lastname=${this.state.lastName}&firstname=${this.state.firstName}&contactnumber=${this.state.contactNumber}&email=${this.state.email}&password=${this.state.newPassword}&user_id=${this.state.id}`);
-
-        if (response.ok) {
-          const result = await response.text();
-          console.log(result); // Log the response
-          sessionStorage.clear();
-          window.location.href = "/";
-          // Update your React component state or perform any additional actions on success
-        } else {
-          console.error("Update failed:", response.statusText);
-          // Handle error in your React component
-        }
-      } catch (error) {
-        console.error("Error:", error);
-        // Handle error in your React component
-      }
+      
     } else {
-      console.log("False");
       this.setState({
-        alertMessage: (
-          <div className="alert alert-danger" role="alert">
-            Passwords Mismatched!
-          </div>
-        ),
+        alertMessage: <div className="alert alert-danger" role="alert">
+                        Password Mismatch!
+                      </div>
       });
     }
   };
+
 
 
   togglePasswordVisibility = () => {
