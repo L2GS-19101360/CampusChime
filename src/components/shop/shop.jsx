cd0b62c0bfff916638e427bcc963477ada3eaff9
zyguel <zyguel@gmail.com>
2023-11-28 23:31:29 +0800

    update

--- src/components/shop/shop.jsx
+++ src/components/shop/shop.jsx
@@ -1,44 +1,58 @@
 import React from 'react';
 import { Form, Button } from 'react-bootstrap'; 
+import './search-bar.css'
+
 
 const Shop = () => {
+ 
+
  return (
  
 <div>
-    <section className="py-0 py-lg-0">
-        <header className="bg-dark py-5">
+    <section className="py-0 py-lg-0 bg-dark">
+    {/*Message Icon and Entry to Messages*/}
+    <button type="button" class="btn py-2 btn-primary position-relative m-4 float-end d-inline">
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-chat-dots" viewBox="0 0 16 16">
+    <path d="M5 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0m4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0m3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2"/>
+    <path d="m2.165 15.803.02-.004c1.83-.363 2.948-.842 3.468-1.105A9.06 9.06 0 0 0 8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6a10.437 10.437 0 0 1-.524 2.318l-.003.011a10.722 10.722 0 0 1-.244.637c-.079.186.074.394.273.362a21.673 21.673 0 0 0 .693-.125zm.8-3.108a1 1 0 0 0-.287-.801C1.618 10.83 1 9.468 1 8c0-3.192 3.004-6 7-6s7 2.808 7 6c0 3.193-3.004 6-7 6a8.06 8.06 0 0 1-2.088-.272 1 1 0 0 0-.711.074c-.387.196-1.24.57-2.634.893a10.97 10.97 0 0 0 .398-2"/>
+    </svg>
+                <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                    99+
+                    <span class="visually-hidden">unread messages</span>
+                </span>
+    </button>
+
+
+    <button type="button" class="btn btn-primary position-relative my-4 py-2 float-end d-inline">
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-basket3-fill" viewBox="0 0 16 16">
+    <path d="M5.757 1.071a.5.5 0 0 1 .172.686L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15.5a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H.5a.5.5 0 0 1-.5-.5v-1A.5.5 0 0 1 .5 6h1.717L5.07 1.243a.5.5 0 0 1 .686-.172zM2.468 15.426.943 9h14.114l-1.525 6.426a.75.75 0 0 1-.729.574H3.197a.75.75 0 0 1-.73-.574z"/>
+    </svg>
+                <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                    99+
+                    <span class="visually-hidden">unread messages</span>
+                </span>
+    </button>
+   
+
+
+        <div class="modal-dialog modal-fullscreen-sm-down">
+        ...
+        </div>
+         {/*Hero Section*/}
+        <header className="py-5">
+            
             <div className="container px-4 px-lg-5 my-5">
             <div className="text-center text-white">
+            
                 <h1 className="display-4 fw-bolder">SEE WHAT'S IN STORE!</h1>
                 <p className="lead fw-normal text-white-50 mb-0">Simplified Shopping Experience!</p>
+                <input type="text" className="search-bar" placeholder="Search"/>
             </div>
             </div>
         </header>
         </section>
        <div className="row">
-            <div className="col-3 bg-success" data-bs-theme="dark" hidden>
-               <p className="fw-bolder p-4 text-white">Test</p>
-               <Form>
-                <Form.Group controlId="formFilter">
-                <Form.Label>Filter Options</Form.Label>
-                <Form.Control type="text" placeholder="Filter by keyword" />
-                </Form.Group>
-
-                <Form.Group controlId="formCategory">
-                <Form.Label>Category</Form.Label>
-                <Form.Control as="select">
-                    <option>Select category...</option>
-                    <option>Category 1</option>
-                    <option>Category 2</option>
-                    {/* Add more categories as needed */}
-                </Form.Control>
-                </Form.Group>
-
-                <Button variant="primary" type="submit">
-                Apply Filters
-                </Button>
-                 </Form>
-            </div>
+           
             
             <div class="offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
             <div class="offcanvas-header">
@@ -52,10 +66,12 @@ const Shop = () => {
             <div className="col-12 bg-danger" data-bs-spy="scroll"  data-bs-target=".container" data-bs-offset="50">
             <div className="px-4 mt-4 d-flex align-items-center">
             <button className="btn" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions">   
+            <section className="d-inline-flex">
             <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-filter-circle-fill" viewBox="0 0 16 16">
             <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16M3.5 5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1 0-1M5 8.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m2 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5"/>
             </svg>
-            <h2 className="d-inline px-2">Filter Results</h2>
+            </section>
+            <h1 className="d-inline-grid px-2">Filter Results</h1>
             </button> 
             </div>
 
