54dfb81e702147a910becfb2d9d077a5d539937c
Jaden-77 <jadenceniza77@gmail.com>
2023-12-03 01:59:44 +0800

    merged me and xyguels shop compnent

--- src/components/home/hero.jsx
+++ src/components/home/hero.jsx
@@ -1,12 +1,15 @@
-import React, { useState } from "react";
-import { Modal, Button, Form } from "react-bootstrap";
+import React, { useState, Suspense } from "react";
 import Shop from "../shop/shop";
+// import SellComponent from './SellComponent';
 import Loader from "../loader/loader";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import SellerProductPage from "../../pages/ProductManagement/SellerProductManage";
+import { Modal, Button, Form } from "react-bootstrap";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import axios from "axios";
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import SellerProductPage from '../../pages/ProductManagement/SellerProductManage';
+
+var user_id = sessionStorage.getItem("userId");
 
 const HeroForHome = () => {
   // State variables
@@ -22,7 +25,10 @@ const HeroForHome = () => {
   // Handle click to navigate to Shop
   const handleShopClick = () => {
     setShowHero(false);
-    setShowShop(true);
+    <Suspense fallback={setIsLoading(true)}>
+      {setShowShop(true)}
+      {setIsLoading(false)}
+    </Suspense>;
   };
 
   // Handle click to initiate the selling process
@@ -35,7 +41,10 @@ const HeroForHome = () => {
       setShowEntrepForm(true); // Show the SellRequestModal
     } else {
       setShowHero(false);
-      setIsLoading(true); // Show Loader for non-customers
+      <Suspense fallback={setIsLoading(true)}>
+        {setShowSellerProductPage(true)}
+        {setIsLoading(false)}
+      </Suspense>;
     }
   };
 
@@ -46,16 +55,20 @@ const HeroForHome = () => {
 
   // Handle sending the request and show a success notification
   const handleSendRequest = (file, productDescription) => {
-    if (!file || !productDescription) {
-      // Handle the case where file or productDescription is not defined
-      console.error("File or productDescription is not defined.");
+    if (!file) {
+      showNotification("Please select a file before submitting.", "error");
+      return;
+    }
+    if (!productDescription) {
+      showNotification("Please provide a product description.", "error");
       return;
     }
 
     const formData = new FormData();
     formData.append("file", file);
     formData.append("productDescription", productDescription);
-    formData.append("user_id", user_id); // Add user_id to the form data
+    formData.append("user_id", user_id);
+    formData.append("requestDate", requestDate);
 
     axios
       .post(
@@ -83,13 +96,29 @@ const HeroForHome = () => {
   };
 
   // Display a notification using Toastify
-  const showNotification = (message) => {
-    toast.success(message, { position: "top-center", autoClose: 2000 });
+  const showNotification = (message, type = "success") => {
+    const options = {
+      position: "top-center",
+      autoClose: 2000,
+    };
+
+    switch (type) {
+      case "success":
+        toast.success(message, options);
+        break;
+      case "error":
+        toast.error(message, options);
+        break;
+      default:
+        toast(message, options);
+        break;
+    }
   };
 
   // SellRequestModal component
   const EntrepRequestModal = ({ onClose, onSendRequest }) => {
     const [file, setFile] = useState(null);
+    const [productDescription, setProductDescription] = useState("");
 
     // Handle file change for uploading the document
     const handleFileChange = (event) => {
@@ -100,13 +129,7 @@ const HeroForHome = () => {
     const handleSubmit = (event) => {
       event.preventDefault();
 
-      if (!file) {
-        console.error("Please select a file before submitting.");
-        showNotification("Please select a file before submitting.", "error");
-        return;
-      }
-
-      onSendRequest(formData);
+      onSendRequest(file, productDescription);
     };
 
     return (
@@ -147,6 +170,7 @@ const HeroForHome = () => {
                 as="textarea"
                 rows={3}
                 placeholder="Enter a brief description of your product..."
+                onChange={(e) => setProductDescription(e.target.value)}
               />
             </Form.Group>
 
@@ -168,49 +192,67 @@ const HeroForHome = () => {
 
   return (
     <div>
-    {showHero && (
-      <section className="HERO-HOME">
-        <div className="container my-5  rounded" style={{backgroundColor: '#C0C0C0'}}>
-        <div className="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
-          <div className="col-lg-7 p-3 p-lg-5 pt-lg-3">
-            <h1 className="display-4 fw-bold lh-1 text-body-emphasis">Welcome to Campus Chime</h1>
-            <p className="lead text-black">Buying and Selling venturing unique businesses, student's best start-up platform!</p>
-            <div className="d-grid gap-2 d-md-flex justify-content-md-start mb-4 mb-lg-3">
-              <button type="button" className="btn btn-success btn-lg px-4 me-md-2 fw-bold"  onClick={() => {
-                  setShowHero(false);
-                  <Suspense fallback={setIsLoading(true)}>
-                  {setShowShop(true)}
-                  {setIsLoading(false)}
-                  </Suspense>
-                 
-                 // setShowShop(true);
-                  
-                }}>Shop Now!</button>
-              <button type="button" className="btn-danger btn btn-lg px-4" onClick={() =>{ 
-                setShowHero(false);
-                setIsLoading(true);
-                <Suspense fallback={setIsLoading(true)}>
-                {setShowSellerProductPage(true)}
-                {setIsLoading(false)}
-                </Suspense>
-                
-                }}>Sell</button>
+      {showHero && (
+        <section className="HERO-HOME">
+          <div
+            className="container my-5  rounded"
+            style={{ backgroundColor: "#C0C0C0" }}
+          >
+            <div className="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
+              <div className="col-lg-7 p-3 p-lg-5 pt-lg-3">
+                <h1 className="display-4 fw-bold lh-1 text-body-emphasis">
+                  Welcome to Campus Chime
+                </h1>
+                <p className="lead text-black">
+                  Buying and Selling venturing unique businesses, student's best
+                  start-up platform!
+                </p>
+                <div className="d-grid gap-2 d-md-flex justify-content-md-start mb-4 mb-lg-3">
+                  <button
+                    type="button"
+                    className="btn btn-success btn-lg px-4 me-md-2 fw-bold"
+                    onClick={handleShopClick}
+                    //placed it all in handShopClick function
+                  >
+                    Shop Now!
+                  </button>
+                  <button
+                    type="button"
+                    className="btn-danger btn btn-lg px-4"
+                    onClick={handleSellClick}
+                    //placed it all in handSellClick function
+                  >
+                    Sell
+                  </button>
+                </div>
+              </div>
+              <div className="col-lg-4 offset-lg-0 p-0 text-center mb-5">
+                <img
+                  className="img-fluid mx-auto"
+                  src="/CAMPUSCHIME ANIMATED LOGO.gif"
+                  alt="CampusChime"
+                  width="480"
+                ></img>
+              </div>
             </div>
           </div>
-          <div className="col-lg-4 offset-lg-0 p-0 text-center mb-5">
-              <img className="img-fluid mx-auto" src="/CAMPUSCHIME ANIMATED LOGO.gif" alt="CampusChime" width="480"></img>
-          </div>
-        </div>
-      </div>
-  </section>
-    )}
-    {isLoading && (
-     <Loader></Loader>
-    )}
-    {showShop && <Shop />}
-    {showSellerProductPage && <SellerProductPage />}
-    {/*showSell && <SellComponent />*/}
-  </div>
+        </section>
+      )}
+      {isLoading && <Loader></Loader>}
+      {showShop && <Shop />}
+      {/* ShowEntrepForm component */}
+      {showEntrepForm && (
+        <EntrepRequestModal
+          onClose={handleCloseSellModal}
+          onSendRequest={handleSendRequest}
+        />
+      )}
+      {showSellerProductPage && <SellerProductPage />}
+      {/*showSell && <SellComponent />*/}
+
+      {/* Toastify notification container */}
+      <ToastContainer />
+    </div>
   );
 };
 
