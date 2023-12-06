0a4fad0445433809d4da985732c43c47948fa7f2
Jaden-77 <jadenceniza77@gmail.com>
2023-12-06 22:53:49 +0800

    minorfix

--- src/components/admin/UserList.jsx
+++ src/components/admin/UserList.jsx
@@ -78,10 +78,7 @@ const UserList = () => {
     <div className="container-fluid mt-3 mb-4 d-flex align-items-center justify-content-center">
       <div className="col-lg-10 mt-4 mt-lg-0">
         <div className="row">
-          <div
-            className="col-md-10"
-            style={{ maxHeight: "85vh", overflowY: "auto" }}
-          >
+          <div className="col-md-10">
             <div className="mb-3 d-flex justify-content-start">
               <Dropdown className="mr-3">
                 <Dropdown.Toggle variant="success" id="role-filter">
@@ -119,97 +116,102 @@ const UserList = () => {
                 </Dropdown.Menu>
               </Dropdown>
             </div>
-            <Table className="table manage-users-top mb-0 text-center">
-              <thead>
-                <tr>
-                  <th className="text-center">User</th>
-                  <th className="text-center">Contact Number</th>
-                  <th className="text-center">Role</th>
-                  <th className="text-center">Status</th>
-                  <th className="text-center">Date Registered</th>
-                  <th className="text-center">Action</th>
-                </tr>
-              </thead>
-              <tbody className="text-center">
-                {users.map((user) => (
-                  <tr key={user.user_id}>
-                    <td className="large-space align-middle">
-                      <div className="title d-flex align-items-center">
-                        <div className="thumb">
-                          {user.user_image === "1" ? (
-                            <LetteredAvatar
-                              name={`${user.firstname} ${user.lastname}`}
-                              size={55}
-                            />
-                          ) : (
-                            <img
-                              src={`http://localhost/campuschime/PHP_files/user_images/${user.user_image}`}
-                              alt={`${user.firstname} ${user.lastname}`}
-                              style={{
-                                width: "55px",
-                                height: "55px",
-                                borderRadius: "50%",
-                                border: "1px solid black",
-                              }}
-                            />
-                          )}
+            <div
+              className="table-responsive mb-0 bg-white p-4 shadow-sm"
+              style={{ maxHeight: "85vh", overflowY: "auto" }}
+            >
+              <Table className="table manage-users-top mb-0 text-center">
+                <thead>
+                  <tr>
+                    <th className="text-center">User</th>
+                    <th className="text-center">Contact Number</th>
+                    <th className="text-center">Role</th>
+                    <th className="text-center">Status</th>
+                    <th className="text-center">Date Registered</th>
+                    <th className="text-center">Action</th>
+                  </tr>
+                </thead>
+                <tbody className="text-center">
+                  {users.map((user) => (
+                    <tr key={user.user_id}>
+                      <td className="large-space align-middle">
+                        <div className="title d-flex align-items-center">
+                          <div className="thumb">
+                            {user.user_image === "1" ? (
+                              <LetteredAvatar
+                                name={`${user.firstname} ${user.lastname}`}
+                                size={55}
+                              />
+                            ) : (
+                              <img
+                                src={`http://localhost/campuschime/PHP_files/user_images/${user.user_image}`}
+                                alt={`${user.firstname} ${user.lastname}`}
+                                style={{
+                                  width: "55px",
+                                  height: "55px",
+                                  borderRadius: "50%",
+                                  border: "1px solid black",
+                                }}
+                              />
+                            )}
+                          </div>
+                          <div
+                            className="candidate-list-title"
+                            style={{
+                              marginTop: "5px",
+                              marginLeft: "15px",
+                              display: "flex",
+                              alignItems: "center",
+                              flexDirection: "column",
+                            }}
+                          >
+                            <h5 className="mb-0">{`${user.firstname} ${user.lastname}`}</h5>
+                            <a
+                              href={`mailto:${user.email}`}
+                              style={{ fontSize: "13px", color: "#888" }}
+                            >
+                              {user.email}
+                            </a>
+                          </div>
                         </div>
+                      </td>
+                      <td className="align-middle">{user.contactnumber}</td>
+                      <td className="align-middle">{user.role}</td>
+                      <td className="candidate-list align-middle">
                         <div
-                          className="candidate-list-title"
+                          className="status-border"
                           style={{
-                            marginTop: "5px",
-                            marginLeft: "15px",
-                            display: "flex",
-                            alignItems: "center",
-                            flexDirection: "column",
+                            borderRadius: "10px",
+                            padding: "5px",
+                            backgroundColor: "#efefef",
+                            color: user.active_status === 1 ? "green" : "red",
+                            fontWeight: "bold",
+                            display: "inline-block",
                           }}
                         >
-                          <h5 className="mb-0">{`${user.firstname} ${user.lastname}`}</h5>
-                          <a
-                            href={`mailto:${user.email}`}
-                            style={{ fontSize: "13px", color: "#888" }}
-                          >
-                            {user.email}
-                          </a>
+                          {user.active_status === 1 ? "Active" : "Inactive"}
                         </div>
-                      </div>
-                    </td>
-                    <td className="align-middle">{user.contactnumber}</td>
-                    <td className="align-middle">{user.role}</td>
-                    <td className="candidate-list align-middle">
-                      <div
-                        className="status-border"
-                        style={{
-                          borderRadius: "10px",
-                          padding: "5px",
-                          backgroundColor: "#efefef",
-                          color: user.active_status === 1 ? "green" : "red",
-                          fontWeight: "bold",
-                          display: "inline-block",
-                        }}
-                      >
-                        {user.active_status === 1 ? "Active" : "Inactive"}
-                      </div>
-                    </td>
-                    <td className="align-middle">
-                      {new Date(user.registered).toLocaleDateString()}
-                    </td>
-                    <td className="align-middle">
-                      <Button
-                        variant={
-                          user.active_status === 1
-                            ? "outline-danger"
-                            : "outline-success"
-                        }
-                        onClick={() => handleUserStatus(user.user_id)}
-                      >
-                        {user.active_status === 1 ? "Deactivate" : "Activate"}
-                      </Button>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </Table>
+                      </td>
+                      <td className="align-middle">
+                        {new Date(user.registered).toLocaleDateString()}
+                      </td>
+                      <td className="align-middle">
+                        <Button
+                          variant={
+                            user.active_status === 1
+                              ? "outline-danger"
+                              : "outline-success"
+                          }
+                          onClick={() => handleUserStatus(user.user_id)}
+                        >
+                          {user.active_status === 1 ? "Deactivate" : "Activate"}
+                        </Button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+            </div>
           </div>
         </div>
       </div>
