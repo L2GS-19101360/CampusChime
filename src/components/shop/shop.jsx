a5bb105fc58d81770149d9af93f177e2068b71f5
zyguel <zyguel@gmail.com>
2023-12-09 09:53:07 +0800

    implemented cartmodal (no backend)

--- src/components/shop/shop.jsx
+++ src/components/shop/shop.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import './search-bar.css'
 import ScrollToTopButton from '../other-nav/gotoTop'
 import ProdFilter from './productFilter';
-
+import CartModal from './cartModal';
 const Shop = () => {
     const [user_id, setUserId] = useState(() => sessionStorage.getItem("userId"));
     const [products, setProducts] = useState([]);
@@ -53,14 +53,14 @@ const Shop = () => {
                 </span>
     </button>
 
-
-    <button type="button" className="btn btn-primary position-relative my-4 py-2 float-end d-inline">
+    <CartModal />
+    <button type="button" className="btn btn-primary position-relative my-4 py-2 float-end d-inline" data-bs-toggle="modal" data-bs-target="#productModal">
     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-basket3-fill" viewBox="0 0 16 16">
     <path d="M5.757 1.071a.5.5 0 0 1 .172.686L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15.5a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H.5a.5.5 0 0 1-.5-.5v-1A.5.5 0 0 1 .5 6h1.717L5.07 1.243a.5.5 0 0 1 .686-.172zM2.468 15.426.943 9h14.114l-1.525 6.426a.75.75 0 0 1-.729.574H3.197a.75.75 0 0 1-.73-.574z"/>
     </svg>
                 <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
                    {cartCount}
-                    <span className="visually-hidden">unread messages</span>
+                    <span className="visually-hidden">added to cart</span>
                 </span>
     </button>
    
